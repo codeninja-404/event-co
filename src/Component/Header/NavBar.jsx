457a2942a0d5b50cb2a37f785b2fdb5e9f3c40b6
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-07 01:31:20 +0600

    added errorpage to the project

--- src/Component/Header/NavBar.jsx
+++ src/Component/Header/NavBar.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
+  const navLinks = (
+    <>
+      <Link to="/">Home</Link>
+      <Link>Home2</Link>
+      <Link>Home3</Link>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -23,48 +32,14 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navLinks}
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+        <ul className="menu menu-horizontal px-1 gap-4">
+         {navLinks}
         </ul>
       </div>
 
@@ -74,7 +49,7 @@ const NavBar = () => {
             <img src="https://i.ibb.co/HzMNzYQ/In-Shot-20231007-002313973.jpg" />
           </div>
         </div>
-        <a className="btn">Button</a>
+        <Link className="btn btn-outline btn-sm text-xs">Log out</Link>
       </div>
     </div>
   );
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,19 @@
-
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            error page
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white  p-8 rounded-lg  text-center my-10">
+        <h1 className="text-4xl font-bold text-red-500 my-10">Error 404</h1>
+        <p className="text-lg text-gray-800 my-4">
+          Oops! The page you are looking for could not be found.
+        </p>
+        <Link href="/" className="mt-4 text-xs btn-sm btn btn-outline text-blue-500  my-4">
+          Go  home 
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
