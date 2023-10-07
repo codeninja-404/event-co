8b285b5ebfe2eb4b95403c8c6b5a9ccab08fe6d9
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-08 00:58:57 +0600

    login fom is working perfectly

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="container h-screen px-2 lg:px-20 mx-auto">
       <div className="   ">
         <div className="hero-content">
           <div className="  w-full max-w-lg  border rounded-xl  shadow-zinc-800 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -32,7 +38,12 @@ const Login = () => {
                 />
               </div>
               <div className="form-control mt-6">
-                <Link className="btn btn-outline text-green-500">Login</Link>
+                <button
+                  className="btn btn-outline text-green-500"
+                  type="submit"
+                >
+                  LOG IN
+                </button>
               </div>
               <div className="flex py-2">
                 <p className="text-white">New here ?</p>
@@ -41,7 +52,7 @@ const Login = () => {
                   to="/register"
                   className="btn btn-xs btn-link text-red-500"
                 >
-                  <button type="submit">Go to register</button>
+                  go to register
                 </Link>
               </div>
             </form>
