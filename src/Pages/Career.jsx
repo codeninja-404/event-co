f56e53a968c7605d2f00be21ddb57c984370245a
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-08 23:57:27 +0600

    career route is complete

--- src/Pages/Career.jsx
+++ src/Pages/Career.jsx
@@ -1,11 +1,144 @@
-
-
 const Career = () => {
-    return (
-        <div className="h-screen container mx-auto px-2 lg:px-20">S
-            
+  return (
+    <div className="h-screen container mx-auto px-2 lg:px-20">
+      <div className="hero min-h-screen  mb-20">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="lg:w-3/6 text-center  lg:text-right">
+            <h1 className="text-4xl text-green-600 font-bold">Start your career with us!</h1>
+            <p className="py-6">
+              Explore a fulfilling career in community and cultural events with
+              us. Join our team to create memorable experiences, promote
+              diversity, and strengthen communities. We offer opportunities for
+              growth and making a meaningful impact while celebrating cultural
+              richness through event planning, marketing, fundraising, and more.
+            </p>
+          </div>
+          <div className="lg:w-3/6 grid gap-6 grid-cols-3">
+            <div className="flex gap-4 flex-col items-center">
+              <div className="   shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/qDNLF1K/photo-2023-10-08-23-51-52.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                    Festival Organizer
+                  </h2>
+                  <p>1 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+            <div className="flex  h-full  gap-4 flex-col items-center">
+              <div className="  shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/m6yHtgx/photo-2023-10-08-23-51-55.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                  Event Coordinator
+                  </h2>
+                  <p>2 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+            <div className="flex h-full  gap-4 flex-col items-center">
+              <div className="  shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/Y8H6yvt/photo-2023-10-08-23-51-58.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                  Fundraising Manager
+                  </h2>
+                  <p>8 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+            <div className="flex h-full  gap-4 flex-col items-center">
+              <div className="  shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/z5mSryf/photo-2023-10-08-23-52-00.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                  Event Planner
+                  </h2>
+                  <p>2 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+            <div className="flex h-full  gap-4 flex-col items-center">
+              <div className="  shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/Xb1XTQg/photo-2023-10-08-23-52-03.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                  Arts Administrator
+                  </h2>
+                  <p>3 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+            <div className="flex h-full  gap-4 flex-col items-center">
+              <div className="  shadow-xl">
+                <figure className="px-5 pb-5">
+                  <img
+                    src="https://i.ibb.co/19Dp2t2/photo-2023-10-08-23-52-04.jpg"
+                    alt="Shoes"
+                    className="rounded-full"
+                  />
+                </figure>
+                <div className=" items-center text-center">
+                  <h2 className="text-xl text-yellow-300">
+                  Volunteer Coordinator
+                  </h2>
+                  <p>5 open jobs</p>
+                </div>
+              </div>
+              <button className="btn btn-sm btn-outline btn-primary">
+                apply
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Career;
